647b6a38d4afe75489c0a67006b9e88e895c44bc
Sorin Chis <kish.sorin@yahoo.com>
2019-03-18 13:58:41 +0200

    server setup - listening on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require('express');
+const port = 5000;
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send('<h1>Working</h1>');
+});
+
+server.listen(port, () => console.log(`Server listening on port ${port}`));
