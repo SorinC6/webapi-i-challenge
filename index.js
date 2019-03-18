43564fd695e681d75868ef9d60ac1255809f49d1
Sorin Chis <kish.sorin@yahoo.com>
2019-03-18 17:13:30 +0200

    added post request

--- index.js
+++ index.js
@@ -57,19 +57,45 @@ server.post('/api/users', (req, res) => {
 
 server.delete('/api/users/:id', (req, res) => {
 	const { id } = req.params;
-   //console.log(id);
+	//console.log(id);
 	db
 		.remove(id)
 		.then((data) => {
 			if (data) {
 				//delete success
-				res.json({message: "succesfully deleted"});
+				res.json({ message: 'succesfully deleted' });
 			} else {
 				//delete not success
 				res.status(404).json({ message: 'The user with the specified ID does not exist.' });
 			}
 		})
 		.catch((err) => res.status(500).json({ error: 'The user could not be removed' }));
+});
+
+server.put('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	const user = req.body;
+
+	if (user.name && user.bio) {
+		db
+			.update(id, user)
+			.then((count) => {
+				if (count) {
+					//200 allgood
+					db.findById(id).then((user) => {
+						res.json(user);
+					});
+				} else {
+					//404 invalid id
+					res.status(404).json({ message: ' invalid id' });
+				}
+			})
+			.catch((err) => {
+				res.status(500).json({ message: 'faild to update user' });
+			});
+	} else {
+		res.status(400).json({ message: 'missing name of bio' });
+	}
 });
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
