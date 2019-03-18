6c118037305099b30e3582e29788a4415b705966
Sorin Chis <kish.sorin@yahoo.com>
2019-03-18 15:50:34 +0200

    added get request for a specified user

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 // implement your API here
 const express = require('express');
 const port = 5000;
+const db = require('./data/db');
 
 const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-	res.send('<h1>Working</h1>');
+	res.json({ message: 'Working' });
+});
+
+server.get('/api/users', (req, res) => {
+	//debugger;
+	db
+		.find()
+		.then((users) => res.json(users))
+		.catch((err) => res.status(500).json({ error: 'There was an error while saving the user to the database' }));
+});
+
+server.get('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	//console.log(id);
+	db
+		.findById(id)
+		.then((user) => {
+         //debugger;
+			if (user) {
+				res.json(user);
+			} else {
+				res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'The user information could not be retrieved.' });
+		});
 });
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
